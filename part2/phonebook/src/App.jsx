a859d554419cb9e7d902179cdf7e58e816b99f76
Ashwani Singh <itsashksingh10@gmail.com>
2025-10-20 10:58:58 +0530

    2.11 completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,22 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Numbers from './components/Numbers'
-import { use } from 'react'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
-
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
+  const sampleData = [
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  ]
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
   const [filter , setFilter] = useState('')
   const [filteredPersons, setFilteredPersons] = useState([])
- 
+  
+  const hook = () =>{
+    axios.get('http://localhost:3001/persons')
+    .then(response => {
+        console.log("promise fulfilled")
+        setPersons(response.data)
+    })
+  }
+  useEffect(hook, [])
+
   // Event Listeners
   const addNumber = (event) =>{
     event.preventDefault()
